079b61da6b242cc5291587826c753409797364fc
Longistamina <dophamthelong03@gmail.com>
2025-08-24 11:09:02 +0700

    added Create categorical series

--- 05_Pandas_DataR_dataframe/01_Pandas_python_style/07_Series_cat_Category_handling.py
+++ 05_Pandas_DataR_dataframe/01_Pandas_python_style/07_Series_cat_Category_handling.py
@@ -6,7 +6,7 @@ and operations on the Series can be performed more efficiently.
 
 
 Flow of contents:
-0. Creat a Categorical Series
+0. Creat a Categorical Series: pd.Series(dtype='category'), .astype('category'), pd.Categorical()
 1. Core attributes: .cat.categories, .cat.codes, .cat.ordered
 2. Adding and Removing: .cat.add_categories(), 
                         .cat.remove_categories(), .cat.remove_unused_categories(), 
@@ -20,3 +20,131 @@ Flow of contents:
 import pandas as pd
 import numpy as np
 
+#---------------------------------------------------------------------------------------------------------------#
+#-------------------------------------- 0. Create a Categorical Series -----------------------------------------#
+#---------------------------------------------------------------------------------------------------------------#
+
+#############################################
+## Using pd.Series() with dtype='category' ##
+#############################################
+
+s_gender = pd.Series(["M", "M", "F", "M", "LGBTQ", "F", "M", "F", "LGBTQ", "M"], dtype = 'category')
+
+print(s_gender)
+# 0        M
+# 1        M
+# 2        F
+# 3        M
+# 4    LGBTQ
+# 5        F
+# 6        M
+# 7        F
+# 8    LGBTQ
+# 9        M
+# dtype: category
+# Categories (3, object): ['F', 'LGBTQ', 'M']
+
+
+######################################
+## Using .astype('category') method ##
+######################################
+
+s_gender = pd.Series(["M", "M", "F", "M", "LGBTQ", "F", "M", "F", "LGBTQ", "M"])
+
+'''
+How .astype('category') works:
+- Converts the Series to a categorical type.
+- Categories are stored as a separate array, which saves memory.
+- Useful for columns with a limited number of unique values.
+- Categories are ordered from A-Z or increasing order.
+'''
+
+s_gender = s_gender.astype('category')
+print(s_gender)
+# 0        M
+# 1        M
+# 2        F
+# 3        M
+# 4    LGBTQ
+# 5        F
+# 6        M
+# 7        F
+# 8    LGBTQ
+# 9        M
+# dtype: category
+# Categories (3, object): ['F', 'LGBTQ', 'M']
+
+
+####################################
+## Using pd.Categorical() method  ##
+####################################
+
+'''
+pd.Categorical() is used to create categorical data from a Series.
+
+It allows you to specify categories and their order, 
+which can be useful for memory efficiency and performance 
+when dealing with columns that have a limited number of unique values.
+
+It's an improved version of .astype('category') that provides more control over the categories.
+
+Note: missing values like NaN or None are not included in the categories by default.
+'''
+
+'''
+By default, the ordered=False, meaning categories are unordered.
+'''
+
+#---------------------------
+## string gender example
+#---------------------------
+
+s_gender = pd.Series(["M", "M", "F", "M", "LGBTQ", "F", "M", "F", "LGBTQ", "M"])
+
+## With ordered = False
+s_gender_categ = pd.Categorical(s_gender, ordered = False)
+print(s_gender_categ)
+# ['M', 'M', 'F', 'M', 'LGBTQ', 'F', 'M', 'F', 'LGBTQ', 'M']
+# Categories (3, object): ['LGBTQ', 'F', 'M']
+
+
+## With ordered = True
+s_gender_categ = pd.Categorical(
+    values = s_gender,
+    categories = ["LGBTQ", "F", "M"],  # Specify the order of categories
+    ordered = True  # Set to True if you want to treat categories as ordered
+)
+print(s_gender_categ)
+# ['M', 'M', 'F', 'M', 'LGBTQ', 'F', 'M', 'F', 'LGBTQ', 'M']
+# Categories (3, object): ['LGBTQ' < 'F' < 'M']
+
+
+#---------------------------
+## numeric example with NaN
+#---------------------------
+
+s_price_levels = pd.Series([1, 1, 3, 2, 5, 2, None, 4, 4, np.nan, 3])
+
+## With ordered = False
+s_price_levels_categ = pd.Categorical(s_price_levels, ordered = False)
+print(s_price_levels_categ)
+# Length: 11
+# Categories (5, float64): [1.0, 2.0, 3.0, 4.0, 5.0]
+'''Here, the NaN and None values are not included in the categories.'''
+
+
+## With ordered = True
+s_price_levels_categ = pd.Categorical(
+    values = s_price_levels,
+    categories = [1, 2, 3, 4, 5], # Define the level
+    ordered = True  # Set to True if you want to treat categories as ordered
+)
+print(s_price_levels_categ)
+# [1, 1, 3, 2, 5, ..., NaN, 4, 4, NaN, 3]
+# Length: 11
+# Categories (5, int64): [1 < 2 < 3 < 4 < 5]
+
+
+#---------------------------------------------------------------------------------------------------------------#
+#------------------------------------------ 1. Core attributes -------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------#
