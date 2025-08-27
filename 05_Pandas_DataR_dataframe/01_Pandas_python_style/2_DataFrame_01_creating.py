695b96cf529ed0c57f102a779a0ddca23b06305a
Longistamina <dophamthelong03@gmail.com>
2025-08-27 20:47:51 +0700

    completed creating dataframe

--- 05_Pandas_DataR_dataframe/01_Pandas_python_style/2_DataFrame_01_creating.py
+++ 05_Pandas_DataR_dataframe/01_Pandas_python_style/2_DataFrame_01_creating.py
@@ -5,10 +5,10 @@ There are many ways to create a DataFrame in Pandas. Here are some of the most c
 2. From a dictionary of lists or ndarrays: {"column_name": list_or_ndarray}
 3. From a structured or record array: np.array([(data1), (data2)], dtype=[("col1", type1), ("col2", type2)])
 4. From a list of dictionaries: [{"col1": val1, "col2": val2}, ...]
-5. From a dictionary of tuples
-6. From a list of namedtuples
-7. From a list of dataclasses
-8. Other constructors
+5. From a dictionary of tuples: {("col1", "col2"): {("row1", "row2"): val, ...}, ...}
+6. From a list of namedtuples: [namedtuple1, namedtuple2, ...]
+7. From a list of dataclasses: [dataclass1, dataclass2, ...]
+8. Other constructors: .from_dict(), .from_records()
 '''
 
 import pandas as pd
@@ -181,3 +181,161 @@ print(df)
 #          a     b     c
 # row_1  1.4   2.3   NaN
 # row_2  5.0  10.0  20.0
+
+
+#-------------------------------------------------------------------------------------------------------------#
+#--------------------------------------- 5. From a dictionary of tuples --------------------------------------#
+#-------------------------------------------------------------------------------------------------------------#
+
+df = pd.DataFrame(
+    {
+        ("a", "b"): {("A", "B"): 1, ("A", "C"): 2},
+        ("a", "a"): {("A", "C"): 3, ("A", "B"): 4},
+        ("a", "c"): {("A", "B"): 5, ("A", "C"): 6},
+        ("b", "a"): {("A", "C"): 7, ("A", "B"): 8},
+        ("b", "b"): {("A", "D"): 9, ("A", "B"): 10},
+    }
+)
+
+print(df)
+#        a              b      
+#        b    a    c    a     b
+# A B  1.0  4.0  5.0  8.0  10.0
+#   C  2.0  3.0  6.0  7.0   NaN
+#   D  NaN  NaN  NaN  NaN   9.0
+
+
+#-------------------------------------------------------------------------------------------------------------#
+#------------------------------------- 6. From a list of namedtuples -----------------------------------------#
+#-------------------------------------------------------------------------------------------------------------#
+
+from collections import namedtuple
+
+###############
+## 2D points ##
+###############
+
+point_2d = namedtuple("Point", "x y")
+
+df = pd.DataFrame(
+    data = [point_2d(0, 0), point_2d(1, 2), point_2d(2, 4), point_2d(3, 6)],
+
+)
+
+print(df)
+#    x  y
+# 0  0  0
+# 1  1  2
+# 2  2  4
+# 3  3  6
+
+###############
+## 3D points ##
+###############
+
+point_3d = namedtuple("Point3D", "x y z")
+
+df = pd.DataFrame(
+    data = [point_3d(0, 0, 0), point_3d(1, 2, 3), point_3d(2, 4, 6), point_3d(3, 6, 9)],
+)
+
+print(df)
+#    x  y  z
+# 0  0  0  0
+# 1  1  2  3
+# 2  2  4  6
+# 3  3  6  9
+
+
+#-------------------------------------------------------------------------------------------------------------#
+#--------------------------------------- 7. From a list of dataclasses ---------------------------------------#
+#-------------------------------------------------------------------------------------------------------------#
+
+from dataclasses import make_dataclass
+
+point_2d = make_dataclass("Point", [("x", int), ("y", int)])
+
+df = pd.DataFrame(
+    data = [point_2d(0, 0), point_2d(3, 5), point_2d(4, 7), point_2d(2, 2)],
+)
+
+print(df)
+#    x  y
+# 0  0  0
+# 1  3  5
+# 2  4  7
+# 3  2  2
+
+
+#-------------------------------------------------------------------------------------------------------------#
+#--------------------------------------- 8. Other constructors -----------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------#
+
+##################
+## .from_dict() ##
+##################
+
+df = pd.DataFrame.from_dict(
+    data = dict([("A", [1, 2, 3]), ("B", [4, 5, 6])])
+)
+print(df)
+#    A  B
+# 0  1  4
+# 1  2  5
+# 2  3  6
+
+df = pd.DataFrame.from_dict(
+    data = dict([("A", [1, 2, 3]), ("B", [4, 5, 6])]),
+    orient = "index", # "index" means the keys of the dictionary will be the row indices
+    columns = ["one", "two", "three"]
+)
+print(df)
+#    one  two  three
+# A    1    2      3
+# B    4    5      6
+
+#####################
+## .from_records() ##
+#####################
+
+# Create an empty structured record
+record = np.zeros(
+    shape = (2,), # 1D array with 2 elements
+    dtype = [("A", "i4"), ("B", "f4"), ("C", "a10")] # Define the data types for each field "A", "B", "C"
+)                                                    # "i4" means 4-byte integer, 
+                                                     # "f4" means 4-byte float, 
+                                                     # "a10" means string of length 10 (bytes
+
+# Fill the structured record with data
+record[:] = [(1, 2.0, "Hello"), (2, 3.0, "World")]
+
+
+# Use .from_records() to create DataFrame
+df = pd.DataFrame.from_records(data = record)
+print(df)
+#    A    B         C
+# 0  1  2.0  b'Hello'
+# 1  2  3.0  b'World'
+
+
+# Use .from_records() to create with index=
+df = pd.DataFrame.from_records(
+    data = record,
+    index = ["first", "second"]
+)
+print(df)
+#         A    B         C
+# first   1  2.0  b'Hello'
+# second  2  3.0  b'World'
+
+
+# Specify index from a specific column
+df = pd.DataFrame.from_records(
+    data = record,
+    index = "C" # Use column "C" as the index
+)
+print(df)
+#           A    B
+# C               
+# b'Hello'  1  2.0
+# b'World'  2  3.0
