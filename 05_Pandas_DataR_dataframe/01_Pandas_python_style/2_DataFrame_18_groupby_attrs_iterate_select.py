a2cab7c59d73bb0501cfbee5df6a34cf50c17fbb
Longistamina <dophamthelong03@gmail.com>
2025-09-03 19:46:11 +0700

    DF 18 preparation

---
+++ 05_Pandas_DataR_dataframe/01_Pandas_python_style/2_DataFrame_18_groupby_attrs_iterate_select.py
@@ -0,0 +1,44 @@
+'''
+1. df.groupby() object attritubes
+
+2. Iterate over groups within a group-by object
+
+3. Select specific groups in a group-by object
+'''
+
+import pandas as pd
+
+df_pokemon = (
+    pd.read_csv(
+        filepath_or_buffer = "05_Pandas_DataR_dataframe/data/pokemon.csv",
+        index_col = "#",
+        dtype = {
+            "Type 1": "category",
+            "Type 2": "category",
+            "Generation": "category",
+            "Legendary": "bool"
+        }
+    )
+    .pipe(lambda df: df.set_axis(df.columns.str.strip().str.replace(r"\s+", "_", regex = True).str.replace(".", ""), axis=1))
+    .assign(Generation = lambda df: df['Generation'].cat.as_ordered())
+)
+
+print(df_pokemon.info())
+# Index: 800 entries, 1 to 721
+# Data columns (total 12 columns):
+#  #   Column      Non-Null Count  Dtype   
+# ---  ------      --------------  -----   
+#  0   Name        800 non-null    object  
+#  1   Type_1      800 non-null    category
+#  2   Type_2      414 non-null    category
+#  3   Total       800 non-null    int64   
+#  4   HP          800 non-null    int64   
+#  5   Attack      800 non-null    int64   
+#  6   Defense     800 non-null    int64   
+#  7   Sp_Atk      800 non-null    int64   
+#  8   Sp_Def      800 non-null    int64   
+#  9   Speed       800 non-null    int64   
+#  10  Generation  800 non-null    category
+#  11  Legendary   800 non-null    bool    
+# dtypes: bool(1), category(3), int64(7), object(1)
+# memory usage: 60.8+ KB
