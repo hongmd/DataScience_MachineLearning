b207ccb289a226d9e37237e33d37b8d99db4f986
Longistamina <dophamthelong03@gmail.com>
2025-11-09 17:22:18 +0900

    completed 29_plotnine_ggplot

--- 05_Pandas_DataR_dataframe/03_DataR_dplyr_style/29_plotnine_ggplot.py
+++ 05_Pandas_DataR_dataframe/03_DataR_dplyr_style/29_plotnine_ggplot.py
@@ -23,3 +23,29 @@ print(tb_baseball >> dr.slice_head(4))
 # 1        Paul_Bako        Catcher      74     215
 # 2  Ramon_Hernandez        Catcher      72     210
 # 3     Kevin_Millar  First_Baseman      72     210
+
+##########################################################################
+##           Use plotnine to draw scatter plot Height ~ Weight          ##
+##########################################################################
+
+(
+    tb_baseball
+    >> pln.ggplot()
+    + pln.theme_seaborn()
+    + pln.geom_point(pln.aes(x = "Weight", y = "Height", color = "Position"))
+    + pln.geom_smooth(pln.aes(x = "Weight", y = "Height"), method = "lm", se = False, color = "black")
+    + pln.ggtitle("Scatter plot Height ~ Weight colored by Position")
+).show()
+
+#########################################################################
+##           Use plotnine to draw boxplot Height ~ Position            ##
+#########################################################################
+
+(
+    tb_baseball
+    >> pln.ggplot()
+    + pln.theme_seaborn()
+    + pln.geom_boxplot(pln.aes(x = "Position", y = "Height", fill = "Position"), notch = True)
+    + pln.coord_flip()
+    + pln.ggtitle("Boxplot Height ~ Position")
+).show()
