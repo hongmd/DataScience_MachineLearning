3a00f70b9d5bce6747da185508fffaaeb9720d9e
Longistamina <dophamthelong03@gmail.com>
2025-11-27 16:41:58 +0900

    prepared for Bolean Indexing

---
+++ 03_Vector_Matrix_Sparse/01_Vector_1D/06_BooleanIndexing_isin.py
@@ -0,0 +1,19 @@
+'''
+Boolean Indexing Boolean Filtering is a powerful technique
+that allows you to filter data based on specific conditions. 
+
+###############################
+
+Flow of contents:
+
+1. Single Condition Examples:
+   + Logic Operators: >, <, >=, <=, ==, !=
+   + np.isin()
+
+2. Negation of Condition: ~ (tilde) operator
+
+3. Combine Multiple Conditions:
+   + & (and),
+   + | (or)
+   + Combine & and |
+'''
