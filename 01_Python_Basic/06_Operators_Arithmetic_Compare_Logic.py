9bc4ceece0a77d2420b2f16b9b82e1e14448a827
Longistamina <dophamthelong03@gmail.com>
2025-11-27 17:31:35 +0900

    added Logic precedence order

--- 01_Python_Basic/06_Operators_Arithmetic_Compare_Logic.py
+++ 01_Python_Basic/06_Operators_Arithmetic_Compare_Logic.py
@@ -10,6 +10,8 @@ a % b  -> get the remainer (chia lay phan du)
 a // b -> get the quotient (chia lay phan nguyen)
 '''
 
+#####################################################################
+
 '''
 a == b #-> equal comparision
 a != b #-> inequal comparision
@@ -20,6 +22,8 @@ a != b #-> inequal comparision
 Comparsion returns  True or False
 '''
 
+#####################################################################
+
 '''
 Chain (Between) Comparisons
 a < b < c
@@ -28,16 +32,40 @@ a < b <= c
 a <= b <= c
 '''
 
+#####################################################################
+
 '''
 a += b is equivalent to a = a + b
 a *= b is equivalent to a = a * b
 '''
 
+#####################################################################
+
 ''' LOGIC
+not - ~: negation
 and - &: return True only when all are True
 or  - |: return False only when all are False
-not - ~: negation
+
+Logic Precedence Order: not -> and -> or
+(menaing NOT logic has the highest precedence, AND logic is next, OR logic has the lowest precedence)
+
+Example:
+A = True
+B = False
+C = True
+result = A or B and C
+       = A or (B and C)  # because AND has higher precedence than OR
+       = A or False
+       = True or False
+       = True
+
+=> That's why we should use parentheses () to explicitly specify the order of operations 
+when combining multiple logical operators.
+
+For example: (A or B) and C
 '''
+
+#####################################################################
 
 ''' IN NOT-IN
 in -> return True if an element is in an interator (Example: 'k'  in 'dek')
