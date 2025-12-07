3b73148d86fa2b9b92c763f306fa766d10c2738c
Longistamina <dophamthelong03@gmail.com>
2025-12-07 11:31:13 +0900

    prepare for the file

---
+++ 03_Vector_Matrix_Sparse/01_Vector_1D/11_logical_methods.py
@@ -0,0 +1,7 @@
+'''
+1. arr.all() ||| np.all(arr): Check if all elements in the array are True (non-zero).
+
+2. arr.any() ||| np.any(arr): Check if any element in the array is True (non-zero).
+
+3. arr.nonzero() ||| np.nonzero(arr): Return the indices of the elements that are non-zero (True).
+'''
