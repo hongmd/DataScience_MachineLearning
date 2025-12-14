1727a0ecbd12f9c794b88099eacc48b660736445
Longistamina <dophamthelong03@gmail.com>
2025-12-14 17:23:56 +0900

    completed up to part 10

--- 03_Vector_Matrix_Sparse/01_Vector_1D/13_string_handlings.py
+++ 03_Vector_Matrix_Sparse/01_Vector_1D/13_string_handlings.py
@@ -3,6 +3,7 @@
    + np.array(list_of_strings)
    + np.array(list_of_strings, dtype='U')
    + np.array(list_of_strings, dtype='Un'): fixed-length Unicode strings of length n
+   + np.array(list_of_strings, dtype=np.dtypes.StringDType()): unlimited-length strings
 
 1. Slicing: np.strings.slice(start, stop, step)
    
@@ -137,6 +138,15 @@ repr(vector_fixed_unicode)
 # "array(['cat', 'dog', 'birds', 'bloun'], dtype='<U5')"
 '''The last string is truncated to "bloun" to fit the fixed length of 5 characters.'''
 
+##############################################################
+## np.array(list_of_strings, dtype=np.dtypes.StringDType()) ##
+##############################################################
+'''dtype=np.dtypes.StringDType() creates an array of unlimited-length strings.'''
+
+vector_unlimited = np.array(["cat", "dog", "birds", "blounder fish"], dtype=np.dtypes.StringDType())
+repr(vector_unlimited)
+# "array(['cat', 'dog', 'birds', 'blounder fish'], dtype=StringDType())"
+
 
 #-------------------------------------------------------------------------------------------------------------------#
 #-------------------------------- 1. Slicing: np.strings.slice(start, stop, step) ----------------------------------#
@@ -214,3 +224,274 @@ print(np.strings.swapcase(vector_tz))
 #  'eUROPE/mADRID' 'aSIA/kUCHING' 'aFRICA/kAMPALA' 'aFRICA/eL_aAIUN'
 #  'eUROPE/sAMARA' 'eUROPE/cOPENHAGEN' 'eUROPE/zAPOROZHYE'
 #  'aMERICA/eIRUNEPE' 'aFRICA/nDJAMENA' 'aMERICA/hAVANA' 'bRAZIL/dEnORONHA']
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------ 3. Stripping -----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+strip_space = np.array(['   leading space', 'trailing space   ', '   both sides   ', 'no_space'])
+strip_other = np.array(['***hello***', 'hellob***', '***hello', 'no_other'])
+
+########################
+## np.strings.strip() ##
+########################
+
+print(np.strings.strip(strip_space))
+# ['leading space' 'trailing space' 'both sides' 'no_space']
+
+print(np.strings.strip(strip_other, '*'))
+# ['hello' 'hellob' 'hello' 'no_other']
+
+#########################
+## np.strings.lstrip() ##
+#########################
+
+print(np.strings.lstrip(strip_space))
+# ['leading space' 'trailing space   ' 'both sides   ' 'no_space']
+
+print(np.strings.lstrip(strip_other, '*'))
+# ['hello***' 'hellob***' 'hello' 'no_other']
+
+#########################
+## np.strings.rstrip() ##
+#########################
+
+print(np.strings.rstrip(strip_space))
+# ['   leading space' 'trailing space' '   both sides' 'no_space']
+
+print(np.strings.rstrip(strip_other, '*'))
+# ['***hello' 'hellob' '***hello' 'no_other']
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------- 4. Length and Count --------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+##########################
+## np.strings.str_len() ##
+##########################
+
+print(np.strings.str_len(vector_tz))
+# [17 13 17 15 13 12 14 15 13 17 17 16 15 14 16]
+
+########################
+## np.strings.count() ##
+########################
+
+print(np.strings.count(vector_tz, 'a'))
+# [3 3 2 1 1 1 4 2 3 1 1 1 3 4 2]
+
+print(np.strings.count(vector_tz, 'Asia'))
+# [1 1 0 0 0 1 0 0 0 0 0 0 0 0 0]
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#-------------------------------------------- 5. Find and Search ---------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+print(vector_tz)
+# ['Asia/Kuala_Lumpur' 'Asia/Sakhalin' 'Antarctica/Vostok' 'Europe/Istanbul'
+#  'Europe/Madrid' 'Asia/Kuching' 'Africa/Kampala' 'Africa/El_Aaiun'
+#  'Europe/Samara' 'Europe/Copenhagen' 'Europe/Zaporozhye'
+#  'America/Eirunepe' 'Africa/Ndjamena' 'America/Havana' 'Brazil/DeNoronha']
+
+#######################
+## np.strings.find() ##
+#######################
+'''Returns the lowest index of the substring if found (from the left), otherwise -1.'''
+
+print(np.strings.find(vector_tz, 'a'))
+# [ 3  3  3 10  8  3  5  5  8 13  8  6  5  6  2]
+
+print(np.strings.find(vector_tz, '###'))
+# [-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1]
+
+########################
+## np.strings.rfind() ##
+########################
+'''Returns the highest index of the substring if found (from the right), otherwise -1.'''
+
+print(np.strings.rfind(vector_tz, 'a'))
+# [ 9  9  9 10  8  3 13 11 12 13  8  6 14 13 15]
+
+print(np.strings.rfind(vector_tz, '###'))
+# [-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1]
+
+########################
+## np.strings.index() ##
+########################
+'''Like find(), but raises an error if the substring is not found.'''
+
+print(np.strings.index(vector_tz, 'a'))
+# [ 3  3  3 10  8  3  5  5  8 13  8  6  5  6  2]
+
+print(np.strings.index(vector_tz, '###'))
+# ValueError: substring not found
+
+#########################
+## np.strings.rindex() ##
+#########################
+'''Like rfind(), but raises an error if the substring is not found.'''
+
+print(np.strings.rindex(vector_tz, 'a'))
+# [ 9  9  9 10  8  3 13 11 12 13  8  6 14 13 15]
+
+print(np.strings.rindex(vector_tz, '###'))
+# ValueError: substring not found
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------- 6. Replacement ----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+print(np.strings.replace(vector_tz, 'Asia', 'ASIA'))
+# ['ASIA/Kuala_Lumpur' 'ASIA/Sakhalin' 'Antarctica/Vostok' 'Europe/Istanbul'
+#  'Europe/Madrid' 'ASIA/Kuching' 'Africa/Kampala' 'Africa/El_Aaiun'
+#  'Europe/Samara' 'Europe/Copenhagen' 'Europe/Zaporozhye'
+#  'America/Eirunepe' 'Africa/Ndjamena' 'America/Havana' 'Brazil/DeNoronha']
+
+print(np.strings.replace(vector_tz, '/', '-'))
+# ['Asia-Kuala_Lumpur' 'Asia-Sakhalin' 'Antarctica-Vostok' 'Europe-Istanbul'
+#  'Europe-Madrid' 'Asia-Kuching' 'Africa-Kampala' 'Africa-El_Aaiun'
+#  'Europe-Samara' 'Europe-Copenhagen' 'Europe-Zaporozhye'
+#  'America-Eirunepe' 'Africa-Ndjamena' 'America-Havana' 'Brazil-DeNoronha']
+
+print(np.strings.replace(vector_tz, 'a', '@', 2)) # Replace first 2 occurrences of 'a' in each elemement
+# ['Asi@/Ku@la_Lumpur' 'Asi@/S@khalin' 'Ant@rctic@/Vostok' 'Europe/Ist@nbul'
+#  'Europe/M@drid' 'Asi@/Kuching' 'Afric@/K@mpala' 'Afric@/El_A@iun'
+#  'Europe/S@m@ra' 'Europe/Copenh@gen' 'Europe/Z@porozhye'
+#  'Americ@/Eirunepe' 'Afric@/Ndj@mena' 'Americ@/H@vana' 'Br@zil/DeNoronh@']
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#------------------------------------------------- 7. Alignment ----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+vector_fruits = np.array(['apple', 'banana', 'cherry', 'date'])
+print(vector_fruits)
+# ['apple' 'banana' 'cherry' 'date']
+
+#########################
+## np.strings.center() ##
+#########################
+
+print(np.strings.center(vector_fruits, 10))
+# ['  apple   ' '  banana  ' '  cherry  ' '   date   ']
+
+print(np.strings.center(vector_fruits, 12, '*'))
+# ['***apple****' '***banana***' '***cherry***' '****date****']
+
+########################
+## np.strings.ljust() ##
+########################
+
+print(np.strings.ljust(vector_fruits, 10))
+# ['apple     ' 'banana    ' 'cherry    ' 'date      ']
+
+print(np.strings.ljust(vector_fruits, 12, '-'))
+# ['apple-------' 'banana------' 'cherry------' 'date--------']
+
+########################
+## np.strings.rjust() ##
+########################
+
+print(np.strings.rjust(vector_fruits, 10))
+# ['     apple' '    banana' '    cherry' '      date']
+
+print(np.strings.rjust(vector_fruits, 12, '+'))
+# ['+++++++apple' '++++++banana' '++++++cherry' '++++++++date']
+
+########################
+## np.strings.zfill() ##
+########################
+
+print(np.strings.zfill(vector_fruits, 10))
+# ['00000apple' '0000banana' '0000cherry' '000000date']
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#----------------------------------------------- 8. Expand Tabs ----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+tabbed_strings = np.array(['Hello\tWorld', 'NumPy\tis\tgreat', 'String\thandling\tin\tPython'])
+print(tabbed_strings)
+# ['Hello\tWorld' 'NumPy\tis\tgreat' 'String\thandling\tin\tPython']
+
+print(np.strings.expandtabs(tabbed_strings, tabsize=4))
+# ['Hello   World' 'NumPy   is  great' 'String  handling    in  Python']
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------- 9. Partitioning ----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+partion_vector = np.array(['apple-banana-cherry', 'date-fig-grape', 'kiwi-mango-papaya'])
+print(partion_vector)
+# ['apple-banana-cherry' 'date-fig-grape' 'kiwi-mango-papaya']
+
+############################
+## np.strings.partition() ##
+############################
+
+print(np.strings.partition(partion_vector, '-'))
+# (array(['apple', 'date', 'kiwi'], dtype='<U5'), 
+#  array(['-', '-', '-'], dtype='<U1'), 
+#  array(['banana-cherry', 'fig-grape', 'mango-papaya'], dtype='<U13'))
+
+'''
+The first array(['apple', 'date', 'kiwi']) contains the substrings before the first occurrence of '-'.
+The second array(['-', '-', '-']) contains the separator itself.
+The third array(['banana-cherry', 'fig-grape', 'mango-papaya']) contains the substrings after the first occurrence of '-'.
+'''
+
+#############################
+## np.strings.rpartition() ##
+#############################
+
+print(np.strings.rpartition(partion_vector, '-'))
+# (array(['apple-banana', 'date-fig', 'kiwi-mango'], dtype='<U12'), 
+#  array(['-', '-', '-'], dtype='<U1'), 
+#  array(['cherry', 'grape', 'papaya'], dtype='<U6'))
+
+'''
+The first array(['apple-banana', 'date-fig', 'kiwi-mango']) contains the substrings before the last occurrence of '-'.
+The second array(['-', '-', '-']) contains the separator itself.
+The third array(['cherry', 'grape', 'papaya']) contains the substrings after the last occurrence of '-'.
+'''
+
+
+#-------------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------- 10. Combination -----------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------------------#
+
+vector1 = np.array(['Hello', 'NumPy', 'String'], dtype=np.dtypes.StringDType())
+vector2 = np.array(['World', 'is great', 'handling in Python'], dtype=np.dtypes.StringDType())
+delimiter = np.array(['_', '_', '_'], dtype=np.dtypes.StringDType())
+
+'''NOTE: use np.dtypes.StringDType() to create string arrays to avoid truncation issues during combination'''
+
+######################
+## np.strings.add() ##
+######################
+
+print(np.strings.add(vector1, vector2))
+# ['HelloWorld' 'NumPyis great' 'Stringhandling in Python']
+
+print(np.strings.add(vector1, delimiter, vector2))
+# ['Hello_' 'NumPy_' 'String_']
+'''DOES NOT WORK AS EXPECTED DUE TO BROADCASTING ISSUE WITH 3 ARRAYS'''
+
+print(np.strings.add(np.strings.add(vector1, delimiter), vector2)) # Must use nested np.strings.add()
+# ['Hello_World' 'NumPy_is great' 'String_handling in Python']
+
+###########################
+## np.strings.multiply() ##
+###########################
+
+print(np.strings.multiply(vector1, 3))
+# ['HelloHelloHello' 'NumPyNumPyNumPy' 'StringStringString']
+
+multiplicands = np.array([1, 2, 3])
+print(np.strings.multiply(vector1, multiplicands))
+# ['Hello' 'NumPyNumPy' 'StringStringString']
