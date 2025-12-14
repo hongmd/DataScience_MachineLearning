5ecd743a77dd23048696c27e79b962734a27d739
Longistamina <dophamthelong03@gmail.com>
2025-12-14 10:27:05 +0900

    prepare for string handling

---
+++ 03_Vector_Matrix_Sparse/01_Vector_1D/12_string_handlings.py
@@ -0,0 +1,78 @@
+'''
+0. Create a 1D string vector:
+   + np.array(list_of_strings)
+   + np.array with dtype='U'
+
+1. Slicing: np.char.slice(start, stop, step)
+   
+2. Case conversions:
+   + np.char.capitalize()
+   + np.char.upper()
+   + np.char.lower()
+   + np.char.title()
+   + np.char.swapcase()
+
+3. Stripping:
+   + np.char.strip()
+   + np.char.lstrip()
+   + np.char.rstrip()
+
+4. Length and Count:
+   + np.char.str_len()
+   + np.char.count()
+
+5. Find and Search:
+   + np.char.find()
+   + np.char.rfind()
+   + np.char.index()
+   + np.char.rindex()
+
+6. Boolean checks:
+   + np.char.isalpha()
+   + np.char.isdigit()
+   + np.char.isnumeric()
+   + np.char.isdecimal()
+   + np.char.isalnum()
+   + np.char.isspace()
+   + np.char.islower()
+   + np.char.isupper()
+   + np.char.istitle()
+
+7. Prefix and Suffix checks:
+   + np.char.startswith()
+   + np.char.endswith()
+
+8. Replacement: np.char.replace()
+
+9. Expand Tabs: np.char.expandtabs()
+
+10. Alignment:
+    + np.char.center()
+    + np.char.ljust()
+    + np.char.rjust()
+    + np.char.zfill()
+
+11. Partitioning:
+    + np.char.partition()
+    + np.char.rpartition()
+    
+12. Combination:
+    + np.char.add()
+    + np.char.multiply()
+
+13. Formating: np.char.mod()
+
+14. Translation: np.char.translate()
+
+15. Encoding and Decoding:
+    + np.char.encode()
+    + np.char.decode()
+
+16. Comparison:
+    + np.char.equal()
+    + np.char.not_equal()
+    + np.char.less()
+    + np.char.less_equal()
+    + np.char.greater()
+    + np.char.greater_equal()
+'''
