c1b8ff0a73e874d74265087d06819fc6bf7e68ab
Longistamina <dophamthelong03@gmail.com>
2025-12-21 22:05:00 +0900

    prepare for file

---
+++ 03_Vector_Matrix_Sparse/01_Vector_1D/14_apply_vectorize_frompyfunc.py
@@ -0,0 +1,7 @@
+'''
+1. np.apply_along_axis(func, axis=0, arr): Applies a function to a 1D vector
+
+2. np.vectorize(func): Vectorizes a function to apply it element-wise on arrays
+
+3. np.frompyfunc(func, nin, nout): Creates a ufunc from a Python function
+'''
