2d7df528eaddf00c4ff04abb239cb5c14bcec796
Longistamina <dophamthelong03@gmail.com>
2026-01-02 22:05:09 +0900

    updated contents

--- 01_Python_Basic/48_logging_loguru_logger.py
+++ 01_Python_Basic/48_logging_loguru_logger.py
@@ -1,9 +1,15 @@
-'''
-logging is a standard Python module that provides a flexible framework for emitting log messages from Python programs. 
+# conda install -c conda-forge loguru
+# pip3 install loguru
 
+'''
 Log message is a text-based record of events that occur within a system, application, or service. 
 It provides a structured or unstructured way to track what happened, when it happened, and potentially why, 
 aiding in debugging, monitoring, and understanding system behavior.
+logging is a standard Python module that provides a flexible framework 
+for emitting log messages from Python programs.
+
+The Loguru library in Python is a modern, user-friendly logging library designed to simplify 
+and enhance the standard Python logging experience. 
 
 There are 5 standard levels of logging, each with a specific purpose:
 1. DEBUG: Detailed information, typically of interest only when diagnosing problems.
@@ -11,7 +17,27 @@ There are 5 standard levels of logging, each with a specific purpose:
 3. WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (not causing error).
 4. ERROR: An indication that due to a more serious problem, the software has not been able to perform some function.
 5. CRITICAL: A very serious error, indicating that the program itself may be unable to continue running.
+
+###############################
+
+Flow of contents:
+
+1. logging
+   + Run with default logging level
+   + Run with DEBUG logging level
+   + Run with INFO logging level
+   + Run with WARNING logging level
+   + Run with ERROR logging level
+   + Run with CRITICAL logging level
+
+2. loguru.logger (from loguru import logger)
+   + loguru.logger
+   + Save the log information into a .txt file for reviewing
 '''
+
+#---------------------------------------------------------------------------------------------#
+#------------------------------------ 1. logging ---------------------------------------------#
+#---------------------------------------------------------------------------------------------#
 
 import logging
 
@@ -93,7 +119,6 @@ logging_with_level(logging.ERROR)
 logging_with_level(logging.CRITICAL)
 # CRITICAL:__main__:This is a critical message.
 
-
 '''
 logging also allows configuring the output format of log messages,
 which can include the timestamp, log level, and message.
@@ -101,3 +126,45 @@ which can include the timestamp, log level, and message.
 However, we will use the loguru library for logging tasks.
 This the logging of loguru is already configured and ready to use.
 '''
+
+
+#---------------------------------------------------------------------------------------------#
+#--------------------- 2. loguru.logger (from loguru import logger) --------------------------#
+#---------------------------------------------------------------------------------------------#
+
+###################
+## loguru.logger ##
+###################
+
+from loguru import logger
+
+logger.debug("This is a debug message.")
+# 2025-07-30 17:34:16.453 | DEBUG    | __main__:<module>:1 - This is a debug message.
+
+
+logger.info("This is an info message.")
+# 2025-07-30 17:34:16.453 | INFO     | __main__:<module>:1 - This is an info message.
+
+
+logger.warning("This is a warning message.")
+# 2025-07-30 17:34:16.454 | WARNING  | __main__:<module>:1 - This is a warning message.
+
+
+logger.error("This is an error message.")
+# 2025-07-30 17:34:16.454 | ERROR    | __main__:<module>:1 - This is an error message.
+
+
+logger.critical("This is a critical message.")
+# 2025-07-30 17:34:16.454 | CRITICAL | __main__:<module>:1 - This is a critical message.
+
+#############################################################
+## Save the log information into a .txt file for reviewing ##
+#############################################################
+
+# Add file sink while keeping the default console output
+logger.add("01_Python_Basic/terminal_logs.txt", 
+           rotation="1 MB",  # Rotate when file reaches 1MB
+           retention="10 days",  # Keep logs for 10 days
+           level="INFO") # Only save the "INFO" level and above
+
+logger.info("This is an info message.")
